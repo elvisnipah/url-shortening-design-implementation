2f02ab3f5c01b8eac55da580b75eabf07d6316ba
Elvis <elvisnipah@gmail.com>
2023-03-03 19:27:46 +0000

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div id="app" className="">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import illustrationWorking from "../assets/illustration-working.svg";
+
+function Hero() {
+  return (
+    <section className="flex flex-col py-4 overflow-hidden">
+      <div className="pl-8 mr-[-40%]">
+        <img src={illustrationWorking} alt="" className="" />
+      </div>
+      <section className="my-10 flex flex-col items-center">
+        <h2 className="font-bold text-5xl text-center">
+          More than just shorter links
+        </h2>
+        <p className="text-gray-400 text-center px-10 py-5 text-lg">
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <button className="bg-[hsl(180,66%,49%)] text-white text-lg font-bold px-9 py-3 rounded-3xl">
+          Get Started
+        </button>
+      </section>
+    </section>
+  );
+}
+
+export default Hero;
